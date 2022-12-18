fe864a16d6389565b3d7d9785978a5b0e93e57b3
haritiana adriano <hei.adriano.4@gmail.com>
2022-12-18 17:48:36 +0300

    fix: over class java

--- src/main/java/com/prog3td/demo/controller/mapper/TeamRestMapper.java
+++ src/main/java/com/prog3td/demo/controller/mapper/TeamRestMapper.java
@@ -1,44 +1,20 @@
 package com.prog3td.demo.controller.mapper;
 
 import com.prog3td.demo.controller.response.Create.CreateTeam;
-import com.prog3td.demo.controller.response.Update.UpdateTeam;
-import com.prog3td.demo.model.Player;
 import com.prog3td.demo.model.Team;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 @Component
 @AllArgsConstructor
 public class TeamRestMapper {
-    private PlayerRestMapper playerMapper;
-    private SponsorRestMapper sponsorMapper;
-    public TeamRest toRest(Team domain){
-        List<Player> PLAYERS = domain.getPlayers();
-        Collections.sort(PLAYERS, Team.playerComparator);
-        return TeamRest.builder()
-                .id(domain.getIdTeam())
-                .name(domain.getName())
-                .players(PLAYERS)
-                .sponsors(domain.getSponsors())
-                .build();
-    }
     public Team toDomain(CreateTeam rest){
         return Team.builder()
                 .name(rest.getName())
                 .players(new ArrayList<>())
                 .sponsors(new ArrayList<>())
-                .build();
-    }
-    public Team toDomain(UpdateTeam rest){
-        return Team.builder()
-                .idTeam(rest.getId())
-                .name(rest.getName())
-                .players(rest.getPlayers())
-                .sponsors(rest.getSponsors())
                 .build();
     }
 }
