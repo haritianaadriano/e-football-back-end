aad0f5ed827fa0e86bf7a5636a9cff5c0e79a685
haritiana adriano <hei.adriano.4@gmail.com>
2023-01-12 10:27:14 +0300

    fix: date type on entity hibernate

--- src/main/java/com/prog3td/demo/model/Match.java
+++ src/main/java/com/prog3td/demo/model/Match.java
@@ -1,14 +1,19 @@
 package com.prog3td.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -25,7 +30,7 @@ public class Match {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long idPlay;
-    private LocalDateTime dateTime;
+    private LocalDate date;
     @JsonIgnore
     private String stadium;
     @ManyToOne
