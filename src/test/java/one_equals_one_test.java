ce9a79f93e82d4c6350e79cc21143ef1b68879f7
haritiana adriano <hei.adriano.4@gmail.com>
2023-01-21 10:04:00 +0300

    feat: one equals one test

---
+++ src/test/java/one_equals_one_test.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class one_equals_one_test {
+    @Test
+    public void test_one_equals_one(){
+        Assertions.assertEquals(1, 1);
+    }
+}
